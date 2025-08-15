af09cc907874dd860f9ebbdad43d941079e13a19
wchresta <34962284+wchresta@users.noreply.github.com>
2025-08-16 00:02:28 +0200

    Improve run and simulate commands

--- cmd/run.go
+++ cmd/run.go
@@ -4,12 +4,17 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"maps"
+	"slices"
+	"strconv"
+	"strings"
+
 	"github.com/spf13/cobra"
 	"github.com/wchresta/passdraw/pkg/runner"
 )
 
 type runCmd struct {
-	passes int
+	availStrings []string
 }
 
 func init() {
@@ -29,40 +34,70 @@ func init() {
 
 	rootCmd.AddCommand(cobraCmd)
 
-	cobraCmd.Flags().IntVar(&cmd.passes, "passes", 0, "Amount of passes to hand out")
+	cobraCmd.Flags().StringSliceVar(&cmd.availStrings, "passes", nil, "Specify availability of passes for partition; format `partition:passes` e.g. `leaders:33`")
 }
 
 func (c *runCmd) Run(cmd *cobra.Command, args []string) {
-	if c.passes <= 0 {
-		cmd.PrintErrln("--passes cannot be 0")
-		return
+	availMap := make(map[runner.Partition]runner.Availability)
+	for _, availStr := range c.availStrings {
+		part, passStr, found := strings.Cut(availStr, ":")
+		if !found {
+			cmd.PrintErrln("--passes must have a `:`. Format `partition:passes`, e.g. `leaders:33`")
+			return
+		}
+		passes, err := strconv.Atoi(passStr)
+		if err != nil {
+			cmd.PrintErrln("--passes must contain a valid number. Format `partition:passes`, e.g. `leaders:33`")
+			return
+		}
+		availMap[runner.Partition(part)] = runner.Availability{
+			Partition: runner.Partition(part),
+			Available: passes,
+		}
 	}
 
-	partition := runner.Partition("All")
+	leaderP := runner.Partition("leader")
+	followP := runner.Partition("follow")
 	r := runner.New([]*runner.User{
-		{Partition: partition, ID: "S0"},
-		{Partition: partition, ID: "S1"},
-		{Partition: partition, ID: "S2"},
-		{Partition: partition, ID: "S3"},
-		{Partition: partition, ID: "S4"},
-		{Partition: partition, ID: "S5"},
-		{Partition: partition, ID: "D0", Deps: []runner.UserID{"S0"}},
-		{Partition: partition, ID: "D1", Deps: []runner.UserID{"S1"}},
-		{Partition: partition, ID: "D2", Deps: []runner.UserID{"S2"}},
-		{Partition: partition, ID: "D3", Deps: []runner.UserID{"S3"}},
-		{Partition: partition, ID: "D4", Deps: []runner.UserID{"S4"}},
-		{Partition: partition, ID: "D5", Deps: []runner.UserID{"S5"}},
+		{Partition: leaderP, ID: "L1"},
+		{Partition: leaderP, ID: "L2"},
+		{Partition: leaderP, ID: "L3"},
+		{Partition: leaderP, ID: "L4"},
+		{Partition: leaderP, ID: "L5"},
+		{Partition: followP, ID: "F1"},
+		{Partition: followP, ID: "F2"},
+		{Partition: followP, ID: "F3"},
+		{Partition: followP, ID: "F4"},
+		{Partition: followP, ID: "F5"},
+		{Partition: leaderP, ID: "La->F1", Deps: []runner.UserID{"F1"}},
+		{Partition: leaderP, ID: "Lb->F2", Deps: []runner.UserID{"F2"}},
+		{Partition: leaderP, ID: "Lc->F3", Deps: []runner.UserID{"F3"}},
+		{Partition: followP, ID: "Fa->L3", Deps: []runner.UserID{"L3"}},
+		{Partition: followP, ID: "Fb->L4", Deps: []runner.UserID{"L4"}},
+		{Partition: followP, ID: "Fc->L5", Deps: []runner.UserID{"L5"}},
+		{Partition: leaderP, ID: "Lx->L1", Deps: []runner.UserID{"L1"}},
+		{Partition: leaderP, ID: "Ly->L2", Deps: []runner.UserID{"L2"}},
+		{Partition: followP, ID: "Fx->F2", Deps: []runner.UserID{"F2"}},
+		{Partition: followP, ID: "Fy->F3", Deps: []runner.UserID{"F3"}},
+		{Partition: leaderP, ID: "Lp->Fx", Deps: []runner.UserID{"Fx->F2"}},
+		{Partition: leaderP, ID: "Lq->Fy,F3", Deps: []runner.UserID{"Fy->F3", "F3"}},
+		{Partition: leaderP, ID: "LC1", Deps: []runner.UserID{"FC1"}},
+		{Partition: followP, ID: "FC1", Deps: []runner.UserID{"LC1"}},
+		{Partition: leaderP, ID: "LC2", Deps: []runner.UserID{"FC2"}},
+		{Partition: followP, ID: "FC2", Deps: []runner.UserID{"LC2"}},
 	})
 
-	solution, err := r.Run([]runner.Availability{{Partition: partition, Available: c.passes}})
+	solution, err := r.Run(slices.Collect(maps.Values(availMap)))
 	if err != nil {
 		cmd.PrintErrf("Run failed: %s", err)
 		return
 	}
 
-	cmd.Printf("Executed Run for %d passes, here are the results:\n", c.passes)
+	cmd.Println("Executed Run for the following availabilities:")
 	for partName, partPass := range solution.Passes {
-		cmd.Printf("Got a total of %d passes for partition %s:\n", len(partPass), partName)
+		a := availMap[partName]
+		cmd.Printf("%s - Handed out %d out of %d passes for partition:\n", partName, len(partPass), a.Available)
+		slices.Sort(partPass)
 		for _, pass := range partPass {
 			cmd.Println(" " + pass)
 		}
--- cmd/simulate.go
+++ cmd/simulate.go
@@ -4,6 +4,9 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"cmp"
+	"fmt"
+	"iter"
 	"maps"
 	"slices"
 
@@ -27,7 +30,7 @@ func init() {
 
 	rootCmd.AddCommand(cobraCmd)
 
-	cobraCmd.Flags().IntVar(&cmd.passes, "passes", 5, "Amount of passes to hand out")
+	cobraCmd.Flags().IntVar(&cmd.passes, "passes", 10, "Amount of passes to hand out")
 	cobraCmd.Flags().IntVar(&cmd.runs, "runs", 1000000, "How many runs")
 }
 
@@ -37,46 +40,83 @@ func (c *simulateCmd) Simulate(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	partition := runner.Partition("All")
-	r := runner.New([]*runner.User{
-		{Partition: partition, ID: "S0"},
-		{Partition: partition, ID: "S1"},
-		{Partition: partition, ID: "S2"},
-		{Partition: partition, ID: "S3"},
-		{Partition: partition, ID: "S4"},
-		{Partition: partition, ID: "S5"},
-		{Partition: partition, ID: "D0", Deps: []runner.UserID{"S0"}},
-		{Partition: partition, ID: "D1", Deps: []runner.UserID{"S1"}},
-		{Partition: partition, ID: "D2", Deps: []runner.UserID{"S2"}},
-		{Partition: partition, ID: "D3", Deps: []runner.UserID{"S3"}},
-		{Partition: partition, ID: "D4", Deps: []runner.UserID{"S4"}},
-		{Partition: partition, ID: "D5", Deps: []runner.UserID{"S5"}},
-		{Partition: partition, ID: "D01", Deps: []runner.UserID{"S0", "S1"}},
-		{Partition: partition, ID: "D12", Deps: []runner.UserID{"S1", "S2"}},
-		{Partition: partition, ID: "D23", Deps: []runner.UserID{"S2", "S3"}},
-	})
+	leaderP := runner.Partition("Leader")
+	followP := runner.Partition("Follow")
+	users := []*runner.User{
+		{Partition: leaderP, ID: "L1"},
+		{Partition: leaderP, ID: "L2"},
+		{Partition: leaderP, ID: "L3"},
+		{Partition: leaderP, ID: "L4"},
+		{Partition: leaderP, ID: "L5"},
+		{Partition: followP, ID: "F1"},
+		{Partition: followP, ID: "F2"},
+		{Partition: followP, ID: "F3"},
+		{Partition: followP, ID: "F4"},
+		{Partition: followP, ID: "F5"},
+		{Partition: leaderP, ID: "La->F1", Deps: []runner.UserID{"F1"}},
+		{Partition: leaderP, ID: "Lb->F2", Deps: []runner.UserID{"F2"}},
+		{Partition: leaderP, ID: "Lc->F3", Deps: []runner.UserID{"F3"}},
+		{Partition: followP, ID: "Fa->L3", Deps: []runner.UserID{"L3"}},
+		{Partition: followP, ID: "Fb->L4", Deps: []runner.UserID{"L4"}},
+		{Partition: followP, ID: "Fc->L5", Deps: []runner.UserID{"L5"}},
+		{Partition: leaderP, ID: "Lx->L1", Deps: []runner.UserID{"L1"}},
+		{Partition: leaderP, ID: "Ly->L2", Deps: []runner.UserID{"L2"}},
+		{Partition: followP, ID: "Fx->F2", Deps: []runner.UserID{"F2"}},
+		{Partition: followP, ID: "Fy->F3", Deps: []runner.UserID{"F3"}},
+		{Partition: leaderP, ID: "Lp->Fx", Deps: []runner.UserID{"Fx->F2"}},
+		{Partition: leaderP, ID: "Lq->Fy,F3", Deps: []runner.UserID{"Fy->F3", "F3"}},
+		{Partition: leaderP, ID: "LC1", Deps: []runner.UserID{"FC1"}},
+		{Partition: followP, ID: "FC1", Deps: []runner.UserID{"LC1"}},
+		{Partition: leaderP, ID: "LC2", Deps: []runner.UserID{"FC2"}},
+		{Partition: followP, ID: "FC2", Deps: []runner.UserID{"LC2"}},
+	}
+	userCountByPart := make(map[runner.Partition]int)
+	for _, u := range users {
+		userCountByPart[u.Partition]++
+	}
+	r := runner.New(users)
 
-	passes := make(map[runner.UserID]int)
+	availMap := make(map[runner.Partition]runner.Availability)
+	availMap[leaderP] = runner.Availability{Partition: leaderP, Available: c.passes / 2}
+	availMap[followP] = runner.Availability{Partition: followP, Available: c.passes - (c.passes / 2)}
+
+	passes := make(map[runner.Partition]map[runner.UserID]int)
 	for i := 0; i <= c.runs; i++ {
-		solution, err := r.Run([]runner.Availability{{Partition: partition, Available: c.passes}})
+		solution, err := r.Run(slices.Collect(maps.Values(availMap)))
 		if err != nil {
 			cmd.PrintErrf("Run failed: %s", err)
 			return
 		}
 
-		for _, partPass := range solution.Passes {
+		for part, partPass := range solution.Passes {
+			if _, ok := passes[part]; !ok {
+				passes[part] = make(map[runner.UserID]int)
+			}
 			for _, pass := range partPass {
-				passes[pass] += 1
+				passes[part][pass] += 1
 			}
 		}
 	}
 
-	passKeys := slices.Collect(maps.Keys(passes))
-	slices.Sort(passKeys)
-
 	cmd.Printf("Performed %d runs; here are the statistics:\n", c.runs)
-	for _, p := range passKeys {
-		n := passes[p]
-		cmd.Printf("User %s got a total of %5d passes; probability of %2.1f%%\n", p, n, float64(n)/float64(c.runs)*100)
+	for part, passes := range sortedKeys(passes) {
+		a := availMap[part]
+		fmt.Printf("Handed out %d passes to %d users in partition %s\n", a.Available, userCountByPart[part], part)
+		totalPasses := 0
+		for u, n := range sortedKeys(passes) {
+			totalPasses += n
+			cmd.Printf("User %-10s got a total of %6d passes; probability of %4.1f%%\n", u, n, float64(n)/float64(c.runs)*100)
+		}
+		fmt.Printf("Handed out a total of %d passes for partition %s\n", totalPasses, part)
+	}
+}
+
+func sortedKeys[Map ~map[K]V, K cmp.Ordered, V any](m Map) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for _, k := range slices.Sorted(maps.Keys(m)) {
+			if !yield(k, m[k]) {
+				return
+			}
+		}
 	}
 }
